65c0f4b2dcaa54292765c44064301b6d936680cf
Farhalinn <farhalinn@gmail.com>
2022-05-27 17:19:59 +0200

    Modification du script pour harmoniser le fichier pour tout les joueurs

--- client_poke_socket.py
+++ client_poke_socket.py
@@ -10,28 +10,52 @@ PORT = 8888 # Port en écoute
 
 # création d'un socket en IPv4
 #SOCK_STREAM : TCP, SOCK_DRGAM : UDP
-print("création socket")
+print("création du socket en cours...\n")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("set blocking")
 
 # on se connecte à une IP et un port en écoute
-print("connection")
-s.connect((HOST, PORT)) 
+print("connexion...\n")
+s.connect((HOST, PORT))
 
-print("on récupère les données..")
-# receive data
+print("connexion réussie !\n")
+
+# Réception du message d'accueil du serveur
+data=s.recv(1024)
+print(data.decode('utf-8'))
+
+#le serveur indique si on est joueur 1 ou 2
 data=s.recv(1024)
 
-# print data
-print(data.decode('utf-8')) # pour l'instant, obliger de faire "enter" pour afficher... on sait pas trop pq
-print("n'importe quoi pour se repérer") # PB ne s'affiche pas dans le terminal client
+if int(data) == 0 : # on est le joueur 1
 
-# let's chat
-while exit == 0 :
-    msg=input("Entrez quelque chose svp")
-    s.sendall(msg.encode('utf-8'))
-    if data.decode('utf-8') == 'exit':
-        exit=1
+    # let's chat
+    while exit == 0 :
+        msg=input("/> ")         # le joueur 1 envoie d'abord
+        s.sendall(msg.encode('utf-8'))
+        
+        if msg.decode('utf-8') == 'exit':            # et exit s'il veut exit
+            exit=1
+
+        data=s.recv(1024)                             # puis écoute
+        print(data.decode('utf-8'))
+        
+        if data.decode('utf-8') == 'exit':            # et exit s'il reçoit exit
+            exit=1
+            
+
+
+else :  # on est le joueur 2
+        # let's chat
+    while exit == 0 :
+        
+        data=s.recv(1024)                           # le joueur 2 écoute d'abord
+        print(data.decode('utf-8'))
+
+        if data.decode('utf-8') == 'exit':          # et exit s'il reçoit exit
+            exit=1
+        else :
+            msg=input("/> ")   # sinon il envoie
+            s.sendall(msg.encode('utf-8'))
         
 # closing socket
 s.close()
